51c528e7c54af9c975ef91177524d58c90ea5199
CyberPokemon <imoncoding@gmail.com>
2025-04-09 02:40:02 +0530

    signup for Citizen added

--- backend/watermanagementbackend/src/main/java/com/example/watermanagementbackend/Repository/CitizenRepo.java
+++ backend/watermanagementbackend/src/main/java/com/example/watermanagementbackend/Repository/CitizenRepo.java
@@ -1,4 +1,10 @@
 package com.example.watermanagementbackend.Repository;
 
-public interface CitizenRepo {
+
+import com.example.watermanagementbackend.Model.Citizen;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CitizenRepo extends JpaRepository<Citizen,Integer> {
 }
--- backend/watermanagementbackend/src/main/java/com/example/watermanagementbackend/Service/CitizenService.java
+++ backend/watermanagementbackend/src/main/java/com/example/watermanagementbackend/Service/CitizenService.java
@@ -1,4 +1,18 @@
 package com.example.watermanagementbackend.Service;
 
+import com.example.watermanagementbackend.Model.Citizen;
+import com.example.watermanagementbackend.Model.Municipality;
+import com.example.watermanagementbackend.Repository.CitizenRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CitizenService {
+
+    @Autowired
+    private CitizenRepo citizenRepo;
+
+    public Citizen register(Citizen citizen) {
+        return citizenRepo.save(citizen);
+    }
 }
