d4016c2144b9598fcf60f970b766e8db8fc9d8f6
CyberPokemon <imoncoding@gmail.com>
2025-04-09 01:03:16 +0530

    Created Citizen Model

---
+++ backend/watermanagementbackend/src/main/java/com/example/watermanagementbackend/Model/Citizen.java
@@ -0,0 +1,81 @@
+package com.example.watermanagementbackend.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Citizen {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+    private String address;
+    private String password;
+
+    private int wardNo;
+    private String municipalityName;
+
+    @Override
+    public String toString() {
+        return "Citizen{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", password='" + password + '\'' +
+                ", wardNo=" + wardNo +
+                ", municipalityName='" + municipalityName + '\'' +
+                '}';
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getWardNo() {
+        return wardNo;
+    }
+
+    public void setWardNo(int wardNo) {
+        this.wardNo = wardNo;
+    }
+
+    public String getMunicipalityName() {
+        return municipalityName;
+    }
+
+    public void setMunicipalityName(String municipalityName) {
+        this.municipalityName = municipalityName;
+    }
+}
