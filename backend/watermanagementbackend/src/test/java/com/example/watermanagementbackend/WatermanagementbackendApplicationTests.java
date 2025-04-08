27252372a13f1f2677ef41841237422a7466adb1
CyberPokemon <imoncoding@gmail.com>
2025-04-09 00:11:26 +0530

    Initial setup

---
+++ backend/watermanagementbackend/src/test/java/com/example/watermanagementbackend/WatermanagementbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.watermanagementbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatermanagementbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
